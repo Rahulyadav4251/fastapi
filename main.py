b14fd03b2b65c41ba1ab9f4241e8c6b6a2d723a9
Rahul <yadav.rahul4251@gmail.com>
2026-01-13 18:02:43 +0530

    .

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+import  sdafasdfasef
 
 app = FastAPI()
 
@@ -15,3 +16,4 @@ async def health_check():
         content={"status": "healthy", "timestamp": datetime.now().isoformat()},
         status_code=200
     )
+
